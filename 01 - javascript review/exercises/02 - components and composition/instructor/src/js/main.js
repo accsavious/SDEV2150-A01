793da6674cb1edfee063048500f5575f40ac2009
Oliver Antoniu <oliver+github@abolish.golf>
2026-01-21 16:44:24 -0700

    add [01 - js review]: ex02 composition/components - the rest of the components

--- 01 - javascript review/exercises/02 - components and composition/instructor/src/js/main.js
+++ 01 - javascript review/exercises/02 - components and composition/instructor/src/js/main.js
@@ -9,11 +9,10 @@
 // STEP 1: Import component modules so they register
 // --------------------------------------------------
 // TODO: Create these files in src/js/components/ and then uncomment imports.
-
 import './components/resource-header.js';
-// import './components/resource-filters.js';
-// import './components/resource-results.js';
-// import './components/resource-details.js';
+import './components/resource-results.js';
+import './components/resource-filters.js';
+import './components/resource-details.js';
 
 // Optional stretch:
 // import './components/resource-item.js';
